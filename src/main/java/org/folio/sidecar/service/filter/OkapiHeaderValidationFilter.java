2419c1ee7d9d8757c7fb09695a73b3749d58a978
Taras Spashchenko <41001394+TarasSpashchenko@users.noreply.github.com>
2025-02-07 17:13:31 +0100

    MODSIDECAR-73: Update OkapiHeaderValidationFilter to use RequestErrorHandler

--- src/main/java/org/folio/sidecar/service/filter/OkapiHeaderValidationFilter.java
+++ src/main/java/org/folio/sidecar/service/filter/OkapiHeaderValidationFilter.java
@@ -6,8 +6,10 @@ import static io.vertx.core.Future.succeededFuture;
 import io.vertx.core.Future;
 import io.vertx.core.MultiMap;
 import io.vertx.ext.web.RoutingContext;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.folio.sidecar.integration.okapi.OkapiHeaders;
+import org.folio.sidecar.utils.RequestErrorHandler;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -16,7 +18,10 @@ import java.util.Map;
 
 @Log4j2
 @Component
+@RequiredArgsConstructor
 public class OkapiHeaderValidationFilter implements IngressRequestFilter {
+
+  private final RequestErrorHandler requestErrorHandler;
 
   @Override
   public Future<RoutingContext> filter(RoutingContext routingContext) {
@@ -24,6 +29,7 @@ public class OkapiHeaderValidationFilter implements IngressRequestFilter {
       validateOkapiHeaders(routingContext.request().headers());
       return succeededFuture(routingContext);
     } catch (IllegalArgumentException e) {
+      requestErrorHandler.handle(routingContext, 400, e.getMessage());
       return failedFuture(e);
     }
   }
