75876f24439355fccdd76c292e8487d7535a7de1
yauhen.v <yauhen_vavilkin@epam.com>
2025-02-21 17:31:37 +0100

    MODSIDECAR-95: adjust x-okapi-permissions processing

---
+++ src/main/java/org/folio/sidecar/service/filter/ModulePermissionPopulator.java
@@ -0,0 +1,55 @@
+package org.folio.sidecar.service.filter;
+
+import static io.vertx.core.Future.succeededFuture;
+import static org.folio.sidecar.utils.CollectionUtils.isEmpty;
+import static org.folio.sidecar.utils.PermissionsUtils.putPermissionsHeaderToRequest;
+import static org.folio.sidecar.utils.RoutingUtils.getScRoutingEntry;
+
+import io.vertx.core.Future;
+import io.vertx.ext.web.RoutingContext;
+import jakarta.enterprise.context.ApplicationScoped;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.folio.sidecar.model.ScRoutingEntry;
+import org.folio.sidecar.service.ModulePermissionsService;
+
+@ApplicationScoped
+@RequiredArgsConstructor
+public class ModulePermissionPopulator implements EgressRequestFilter {
+
+  private final ModulePermissionsService modulePermissionsService;
+
+  @Override
+  public int getOrder() {
+    return HIGHEST_PRECEDENCE;
+  }
+
+  @Override
+  public Future<RoutingContext> filter(RoutingContext ctx) {
+    if (shouldSkip(ctx)) {
+      return succeededFuture(ctx);
+    }
+
+    return modulePermissionsService.getPermissions()
+      .map(permissions -> findDesiredPermissions(getScRoutingEntry(ctx), permissions))
+      .map(permissions -> putPermissionsHeaderToRequest(ctx, permissions));
+  }
+
+  private static boolean shouldSkip(RoutingContext ctx) {
+    var scRoutingEntry = getScRoutingEntry(ctx);
+    return scRoutingEntry == null
+      || scRoutingEntry.getRoutingEntry() == null
+      || isEmpty(scRoutingEntry.getRoutingEntry().getPermissionsDesired());
+  }
+
+  private static List<String> findDesiredPermissions(ScRoutingEntry re, List<String> modulePermissions) {
+    var routingEntry = re.getRoutingEntry();
+    var preparedForMatching = routingEntry.getPermissionsDesired().stream()
+      .map(permission -> permission.replace("*", ""))
+      .toList();
+    return modulePermissions.stream()
+      .filter(permission -> preparedForMatching.stream().anyMatch(permission::startsWith))
+      .toList();
+  }
+}
+
--- src/main/java/org/folio/sidecar/service/routing/RoutingService.java
+++ src/main/java/org/folio/sidecar/service/routing/RoutingService.java
@@ -5,6 +5,7 @@ import static org.apache.commons.lang3.StringUtils.containsIgnoreCase;
 import static org.folio.sidecar.service.routing.RoutingService.ModuleType.PRIMARY;
 import static org.folio.sidecar.service.routing.RoutingService.ModuleType.REQUIRED;
 import static org.folio.sidecar.utils.CollectionUtils.isEmpty;
+import static org.folio.sidecar.utils.PermissionsUtils.extractPermissions;
 import static org.folio.sidecar.utils.RoutingUtils.dumpHeaders;
 import static org.folio.sidecar.utils.RoutingUtils.dumpUri;
 
@@ -26,6 +27,7 @@ import org.folio.sidecar.configuration.properties.RoutingHandlerProperties;
 import org.folio.sidecar.integration.am.ApplicationManagerService;
 import org.folio.sidecar.integration.am.model.ModuleBootstrap;
 import org.folio.sidecar.service.ErrorHandler;
+import org.folio.sidecar.service.ModulePermissionsService;
 
 @Log4j2
 @ApplicationScoped
@@ -37,6 +39,7 @@ public class RoutingService {
   private final EgressRequestHandler egressRequestHandler;
   private final IngressRequestHandler ingressRequestHandler;
   private final RequestMatchingService requestMatchingService;
+  private final ModulePermissionsService modulePermissionsService;
   private final RoutingHandlerProperties routingHandlerProperties;
 
   private final Map<String, ModuleType> knownModules = new HashMap<>();
@@ -66,6 +69,8 @@ public class RoutingService {
     log.debug("Loaded module bootstrap: {}", moduleBootstrap);
 
     requestMatchingService.bootstrapModule(moduleBootstrap);
+
+    modulePermissionsService.putPermissions(extractPermissions(moduleBootstrap));
 
     var routerRequestHandler = createRequestHandler();
 
@@ -102,6 +107,7 @@ public class RoutingService {
     return moduleBootstrap -> {
       if (type == PRIMARY) {
         requestMatchingService.updateIngressRoutes(moduleBootstrap.getModule());
+        modulePermissionsService.putPermissions(extractPermissions(moduleBootstrap));
         return;
       }
 
