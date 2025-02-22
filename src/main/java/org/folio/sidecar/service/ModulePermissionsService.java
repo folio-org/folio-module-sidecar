20afc9303e6fbab65c207c975b9eb02366d478ac
yauhen.v <yauhen_vavilkin@epam.com>
2025-02-22 10:19:16 +0100

    MODSIDECAR-94: replace AtomicReference by volatile

--- src/main/java/org/folio/sidecar/service/ModulePermissionsService.java
+++ src/main/java/org/folio/sidecar/service/ModulePermissionsService.java
@@ -5,24 +5,22 @@ import static io.vertx.core.Future.succeededFuture;
 import io.vertx.core.Future;
 import jakarta.enterprise.context.ApplicationScoped;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @ApplicationScoped
 public class ModulePermissionsService {
 
-  private final AtomicReference<List<String>> permissionsCache = new AtomicReference<>(List.of());
+  private volatile List<String> permissionsCache = List.of();
 
   public Future<List<String>> getPermissions() {
-    var permissions = permissionsCache.get();
-    log.debug("Returning permissions from cache: {}", permissions);
-    return succeededFuture(permissions);
+    log.debug("Returning permissions from cache: {}", permissionsCache);
+    return succeededFuture(permissionsCache);
   }
 
   public Future<Void> putPermissions(List<String> permissions) {
     log.debug("Updating permissions cache: {}", permissions);
-    permissionsCache.set(List.copyOf(permissions));
+    permissionsCache = List.copyOf(permissions);
     return succeededFuture();
   }
 }
