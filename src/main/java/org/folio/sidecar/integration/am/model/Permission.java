535c99d95f69bc5937d39778783e11598a69f63f
yauhen.v <yauhen_vavilkin@epam.com>
2025-02-25 09:39:48 +0100

    MODSIDECAR-94: add annotations

--- src/main/java/org/folio/sidecar/integration/am/model/Permission.java
+++ src/main/java/org/folio/sidecar/integration/am/model/Permission.java
@@ -1,12 +1,16 @@
 package org.folio.sidecar.integration.am.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import lombok.Data;
 
 @Data
+@RegisterForReflection
 public class Permission {
 
   /**
    * Permission name.
    */
+  @JsonProperty
   private String permissionName;
 }
