024abd16fde26444454d62feffdf287201d1da09
yauhen.v <yauhen_vavilkin@epam.com>
2025-02-27 19:06:32 +0100

    MODSIDECAR-94: add x-okapi-permissions adjustments

--- src/main/java/org/folio/sidecar/service/ModulePermissionsService.java
+++ src/main/java/org/folio/sidecar/service/ModulePermissionsService.java
@@ -4,23 +4,23 @@ import static io.vertx.core.Future.succeededFuture;
 
 import io.vertx.core.Future;
 import jakarta.enterprise.context.ApplicationScoped;
-import java.util.List;
+import java.util.Set;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @ApplicationScoped
 public class ModulePermissionsService {
 
-  private volatile List<String> permissionsCache = List.of(); // NOSONAR
+  private volatile Set<String> permissionsCache = Set.of(); // NOSONAR
 
-  public Future<List<String>> getPermissions() {
+  public Future<Set<String>> getPermissions() {
     log.debug("Returning permissions from cache: {}", permissionsCache);
     return succeededFuture(permissionsCache);
   }
 
-  public Future<Void> putPermissions(List<String> permissions) {
+  public Future<Void> putPermissions(Set<String> permissions) {
     log.debug("Updating permissions cache: {}", permissions);
-    permissionsCache = List.copyOf(permissions);
+    permissionsCache = Set.copyOf(permissions);
     return succeededFuture();
   }
 }
