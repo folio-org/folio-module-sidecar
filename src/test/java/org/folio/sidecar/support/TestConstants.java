3a1ca28ad43d39ed9c0b4c8084efbbe9958a2145
Dmytro Tkachenko <dmytro_tkachenko@epam.com>
2025-03-20 19:16:18 +0200

    egress handler test

--- src/test/java/org/folio/sidecar/support/TestConstants.java
+++ src/test/java/org/folio/sidecar/support/TestConstants.java
@@ -18,17 +18,14 @@ import org.folio.sidecar.integration.tm.TenantManagerClientProperties;
 public class TestConstants {
 
   public static final String TENANT_NAME = "testtenant";
-  public static final String FOLIO_ENV = "folio";
   public static final String TENANT_ID = "90b113f0-4e98-45f7-bccc-cf318e13a9bc";
   public static final String USER_ID = "00000000-0000-0000-0000-111111111111";
   public static final UUID TENANT_UUID = UUID.fromString(TENANT_ID);
   public static final String MODULE_NAME = "mod-foo";
-  public static final String SIDECAR_NAME = "sc-mod-foo";
   public static final String GATEWAY_URL = "http://api-gateway:8000";
   public static final String MODULE_VERSION = "0.2.1";
   public static final String MODULE_ID = MODULE_NAME + "-" + MODULE_VERSION;
   public static final String MODULE_URL = "http://sc-foo:8081";
-  public static final String MODULE_URL_TLS = "https://sc-foo:8081";
   public static final String MODULE_HEALTH_PATH = "/admin/health";
   public static final String APPLICATION_ID = "application-0.0.1";
   public static final ModuleBootstrap MODULE_BOOTSTRAP =
@@ -38,9 +35,13 @@ public class TestConstants {
   public static final String AUTH_TOKEN = "dGVzdC1hY2Nlc3MtdG9rZW4=";
   public static final String REFRESH_TOKEN = "dGVzdC1yZWZyZXNoLXRva2Vu";
   public static final String SYS_TOKEN = "dGVzdC1zeXN0ZW0tYWNjZXNzLXRva2Vu";
+  public static final String USER_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9";
+  public static final String SYS_USER_TOKEN = "eyJ0eXAiOidFV1QiLCJhbGc3O5JIUzI1NiJ7";
   public static final TokenResponse TOKEN_RESPONSE = new TokenResponse(AUTH_TOKEN, REFRESH_TOKEN, 10L);
   public static final ClientCredentials
     LOGIN_CLIENT_CREDENTIALS = ClientCredentials.of(TENANT_NAME + "-login-app", "secret");
+
+  public static final String REQUEST_ID = "000000/foo";
 
   public static final ModuleProperties MODULE_PROPERTIES =
     new ModuleProperties(MODULE_ID, MODULE_NAME, MODULE_VERSION, MODULE_URL, MODULE_HEALTH_PATH);
