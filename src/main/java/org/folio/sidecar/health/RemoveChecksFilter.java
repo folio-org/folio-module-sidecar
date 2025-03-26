428268d86c4bda67fe685503bbd2a13b98ca99e3
Dmytro Tkachenko <dmytro_tkachenko@epam.com>
2025-03-26 18:03:51 +0200

    filters to remove 'checks' / 'data' from health check payload

---
+++ src/main/java/org/folio/sidecar/health/RemoveChecksFilter.java
@@ -0,0 +1,33 @@
+package org.folio.sidecar.health;
+
+import static org.folio.sidecar.health.HealthCheckPayloadConstants.CHECKS_KEY;
+import static org.folio.sidecar.health.HealthCheckPayloadConstants.STATUS_KEY;
+
+import io.smallrye.health.api.HealthContentFilter;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@ApplicationScoped
+public class RemoveChecksFilter implements HealthContentFilter {
+
+  @Override
+  public JsonObject filter(JsonObject payload) {
+    if (payload == null) {
+      return null;
+    }
+
+    var result = payload;
+    if (result.containsKey(CHECKS_KEY)) {
+      result = Json.createObjectBuilder()
+        .add(STATUS_KEY, result.getString(STATUS_KEY))
+        .build();
+
+      log.debug("Health checks under \"{}\" attribute removed from the health response", CHECKS_KEY);
+    }
+
+    return result;
+  }
+}
