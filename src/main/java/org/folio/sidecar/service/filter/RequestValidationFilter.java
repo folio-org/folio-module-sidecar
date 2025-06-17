a5925b06e7f379084f098a35a76200a9b7f594b5
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2025-06-17 16:11:11 +0300

    MODSIDECAR-125 - use header to check body

--- src/main/java/org/folio/sidecar/service/filter/RequestValidationFilter.java
+++ src/main/java/org/folio/sidecar/service/filter/RequestValidationFilter.java
@@ -5,6 +5,7 @@ import static io.vertx.core.Future.succeededFuture;
 import static org.folio.sidecar.service.filter.IngressFilterOrder.REQUEST_VALIDATION;
 
 import io.vertx.core.Future;
+import io.vertx.core.MultiMap;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.ext.web.RoutingContext;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -17,9 +18,11 @@ import lombok.extern.log4j.Log4j2;
 @RequiredArgsConstructor
 public class RequestValidationFilter implements IngressRequestFilter {
 
+  private static final String CONTENT_LENGTH_HEADER = "Content-Length";
+
   @Override
   public Future<RoutingContext> filter(RoutingContext rc) {
-    if (HttpMethod.GET.equals(rc.request().method()) && !rc.body().isEmpty()) {
+    if (HttpMethod.GET.equals(rc.request().method()) && isContentLengthHeaderExist(rc.request().headers())) {
       return failedFuture(new BadRequestException("GET requests should not have a body"));
     }
     return succeededFuture(rc);
@@ -28,5 +31,13 @@ public class RequestValidationFilter implements IngressRequestFilter {
   @Override
   public int getOrder() {
     return REQUEST_VALIDATION.getOrder();
+  }
+
+  private boolean isContentLengthHeaderExist(MultiMap headers) {
+    if (headers.contains(CONTENT_LENGTH_HEADER)) {
+      var length = headers.get(CONTENT_LENGTH_HEADER);
+      return Integer.parseInt(length) > 0;
+    }
+    return false;
   }
 }
--- src/test/java/org/folio/sidecar/service/filter/RequestValidationFilterTest.java
+++ src/test/java/org/folio/sidecar/service/filter/RequestValidationFilterTest.java
@@ -4,9 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.folio.sidecar.service.filter.IngressFilterOrder.REQUEST_VALIDATION;
 import static org.mockito.Mockito.when;
 
+import io.vertx.core.MultiMap;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServerRequest;
-import io.vertx.ext.web.RequestBody;
 import io.vertx.ext.web.RoutingContext;
 import jakarta.ws.rs.BadRequestException;
 import org.folio.support.types.UnitTest;
@@ -21,16 +21,29 @@ class RequestValidationFilterTest {
 
   @Mock private RoutingContext routingContext;
   @Mock private HttpServerRequest request;
-  @Mock private RequestBody requestBody;
+  @Mock private MultiMap headers;
 
   private final RequestValidationFilter filter = new RequestValidationFilter();
 
   @Test
-  void filter_positive_noBodyForGetRequest() {
+  void filter_positive_noBodyForGetRequestWhenContentLengthDoesNotExist() {
     when(routingContext.request()).thenReturn(request);
     when(routingContext.request().method()).thenReturn(HttpMethod.GET);
-    when(routingContext.body()).thenReturn(requestBody);
-    when(routingContext.body().isEmpty()).thenReturn(true);
+    when(request.headers()).thenReturn(headers);
+    when(headers.contains("Content-Length")).thenReturn(false);
+
+    var result = filter.filter(routingContext);
+
+    assertThat(result.succeeded()).isTrue();
+  }
+
+  @Test
+  void filter_positive_noBodyForGetRequestWhenContentLengthIsZero() {
+    when(routingContext.request()).thenReturn(request);
+    when(routingContext.request().method()).thenReturn(HttpMethod.GET);
+    when(request.headers()).thenReturn(headers);
+    when(headers.contains("Content-Length")).thenReturn(true);
+    when(headers.get("Content-Length")).thenReturn("0");
 
     var result = filter.filter(routingContext);
 
@@ -41,8 +54,9 @@ class RequestValidationFilterTest {
   void filter_negative_bodyPresentForGetRequest() {
     when(routingContext.request()).thenReturn(request);
     when(routingContext.request().method()).thenReturn(HttpMethod.GET);
-    when(routingContext.body()).thenReturn(requestBody);
-    when(routingContext.body().isEmpty()).thenReturn(false);
+    when(request.headers()).thenReturn(headers);
+    when(headers.contains("Content-Length")).thenReturn(true);
+    when(headers.get("Content-Length")).thenReturn("1");
 
     var result = filter.filter(routingContext);
 
