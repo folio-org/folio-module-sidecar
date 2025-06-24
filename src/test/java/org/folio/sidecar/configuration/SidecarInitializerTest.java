b2f74339bf5a6c457b3dfa450715bee029a294da
Dmytro Tkachenko <dmytro_tkachenko@epam.com>
2025-06-24 17:00:38 +0300

    unit tests

--- src/test/java/org/folio/sidecar/configuration/SidecarInitializerTest.java
+++ src/test/java/org/folio/sidecar/configuration/SidecarInitializerTest.java
@@ -1,10 +1,13 @@
 package org.folio.sidecar.configuration;
 
-import static org.mockito.Mockito.verify;
+import static io.vertx.core.Future.succeededFuture;
+import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 import io.vertx.ext.web.Router;
 import org.folio.sidecar.configuration.properties.SidecarProperties;
+import org.folio.sidecar.service.TenantService;
 import org.folio.sidecar.service.routing.RoutingService;
 import org.folio.sidecar.startup.SidecarInitializer;
 import org.folio.support.types.UnitTest;
@@ -24,15 +27,24 @@ class SidecarInitializerTest {
   @Mock private Router router;
   @Mock private RoutingService routingService;
   @Mock private SidecarProperties sidecarProperties;
+  @Mock private TenantService tenantService;
 
   @AfterEach
   void tearDown() {
-    verifyNoMoreInteractions(routingService);
+    verifyNoMoreInteractions(routingService, tenantService, sidecarProperties);
   }
 
   @Test
   void onStart_positive() {
+    when(sidecarProperties.getName()).thenReturn("sc-mod-foo");
+    when(routingService.init(router)).thenReturn(succeededFuture());
+    when(tenantService.init()).thenReturn(succeededFuture());
+
+    var initOrder = inOrder(routingService, tenantService);
+
     routerConfiguration.onStart(router);
-    verify(routingService).init(router);
+
+    initOrder.verify(routingService).init(router);
+    initOrder.verify(tenantService).init();
   }
 }
