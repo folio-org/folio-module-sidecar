9090c970bb5a723301eaa2a89422d569211da914
Julian Ladisch <julianladisch@users.noreply.github.com>
2025-08-07 19:45:45 +0200

    MODSIDECAR-140: Use SECURE_STORE_ENV, not ENV, for secure store key
    https://folio-org.atlassian.net/browse/MODSIDECAR-140
    
    Summary:
    
    Use the SECURE_STORE_ENV environment variable, not the ENV environment variable, as the first component of the secure store key.
    
    Description:
    
    It’s a well known pattern to run a single Kafka cluster for multiple flower releases: https://github.com/folio-org/rfcs/blob/master/text/0002-kafka-tenant-collection-topics.md#migration-to-new-module-version
    
    The ENV environment variable is used as the first component of the Kafka topic. Setting ENV to the flower name (like ramsons or sunflower) ensures that topics are produced and consumed by modules of the same flower release only.
    
    Using ENV for the secure store key is wrong because a password doesn’t change when migrating a tenant from ramsons to sunflower.
    
    There can be a need to separate the entries in the secure store, for example using prod and test, but not by using the flower names from the ENV environment variable.
    
    Fix, see https://folio-org.atlassian.net/browse/FOLIO-4344 :
    
    * replace ENV by SECURE_STORE_ENV environment variable if used for secure store key
    * fall back to ENV if SECURE_STORE_ENV is not defined to ensure upgrade compatibility for Ramsons CSP and Sunflower CSP; this fall back is not used for Trillium
    * fall back to folio if neither SECURE_STORE_ENV nor ENV are defined (the same as the ENV default to ensure compatibility)
    * module’s README explains SECURE_STORE_ENV

--- src/main/java/org/folio/sidecar/utils/FolioEnvironment.java
+++ src/main/java/org/folio/sidecar/utils/FolioEnvironment.java
@@ -1,29 +1,33 @@
 package org.folio.sidecar.utils;
 
-import static java.lang.System.getProperty;
-import static java.lang.System.getenv;
-import static org.apache.commons.lang3.StringUtils.isNotEmpty;
-
+import java.util.Map;
+import java.util.Properties;
 import lombok.experimental.UtilityClass;
+import org.apache.commons.lang3.StringUtils;
 
 @UtilityClass
 public class FolioEnvironment {
 
+  private static String secureStoreEnvName = getSecureStoreEnvName(System.getenv(), System.getProperties());
+
   /**
-   * Return folio env name from environment or system properties as {@link String} object.
-   *
-   * @return folio env name.
+   * Return the first non-empty value: SECURE_STORE_ENV from env, secure.store.env from properties,
+   * ENV from env, env from properties, "folio".
    */
-  public static String getFolioEnvName() {
-    var env = getenv("ENV");
-    if (isNotEmpty(env)) {
-      return env;
-    } else {
-      var propertyEnv = getProperty("env");
-      if (isNotEmpty(propertyEnv)) {
-        return propertyEnv;
-      }
-    }
-    return "folio";
+  public static String getSecureStoreEnvName(Map<String, String> env, Properties properties) {
+    return StringUtils.firstNonEmpty(
+        env.get("SECURE_STORE_ENV"),
+        properties.getProperty("secure.store.env"),
+        env.get("ENV"),
+        properties.getProperty("env"),
+        "folio");
+  }
+
+  /**
+   * Return the first non-empty value: SECURE_STORE_ENV environment variable, secure.store.env system property,
+   * ENV environment variable, env system property, "folio".
+   */
+  public static String getSecureStoreEnvName() {
+    return secureStoreEnvName;
   }
 }
