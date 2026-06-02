d02c373e92153f5235a53afe8cadc0add898b7f3
Dmytro Tkachenko <dmytro_tkachenko@epam.com>
2026-06-02 12:18:34 +0300

    fix(handler): update path matching to use Strings.CI for case-insensitive comparison

--- src/main/java/org/folio/sidecar/service/routing/handler/TraceHeadersHandler.java
+++ src/main/java/org/folio/sidecar/service/routing/handler/TraceHeadersHandler.java
@@ -1,16 +1,16 @@
 package org.folio.sidecar.service.routing.handler;
 
-import static org.apache.commons.lang3.StringUtils.containsIgnoreCase;
 import static org.folio.sidecar.utils.CollectionUtils.isEmpty;
 import static org.folio.sidecar.utils.RoutingUtils.dumpHeaders;
 import static org.folio.sidecar.utils.RoutingUtils.dumpUri;
 
-import io.vertx.codegen.annotations.Nullable;
 import io.vertx.core.Handler;
 import io.vertx.ext.web.RoutingContext;
 import java.util.Collection;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.Strings;
+import org.jspecify.annotations.Nullable;
 
 @Log4j2
 @RequiredArgsConstructor
@@ -38,6 +38,6 @@ public class TraceHeadersHandler implements Handler<RoutingContext> {
   }
 
   private boolean pathMatched(@Nullable String path) {
-    return isEmpty(paths) || paths.stream().anyMatch(s -> containsIgnoreCase(path, s));
+    return isEmpty(paths) || paths.stream().anyMatch(s -> Strings.CI.contains(path, s));
   }
 }
