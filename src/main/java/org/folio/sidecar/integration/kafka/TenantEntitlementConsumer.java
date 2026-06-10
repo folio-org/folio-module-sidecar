28046f05fbf52987a8d5be8c6b3e98d2fc07640b
okuzminov <oleksii_kuzminov@epam.com>
2026-06-10 12:25:45 +0200

    feat(sidecar): apply applicationId in entitlement consumer

--- src/main/java/org/folio/sidecar/integration/kafka/TenantEntitlementConsumer.java
+++ src/main/java/org/folio/sidecar/integration/kafka/TenantEntitlementConsumer.java
@@ -7,8 +7,10 @@ import jakarta.enterprise.context.ApplicationScoped;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.folio.sidecar.integration.am.ApplicationManagerService;
 import org.folio.sidecar.integration.kafka.TenantEntitlementEvent.Type;
 import org.folio.sidecar.service.TenantService;
+import org.folio.sidecar.service.routing.lookup.EgressRoutingLookup;
 
 @Log4j2
 @ApplicationScoped
@@ -16,6 +18,8 @@ import org.folio.sidecar.service.TenantService;
 public class TenantEntitlementConsumer {
 
   private final TenantService tenantService;
+  private final ApplicationManagerService applicationManagerService;
+  private final EgressRoutingLookup egressRoutingLookup;
 
   @Incoming("entitlement")
   public void consume(TenantEntitlementEvent event) {
@@ -29,10 +33,17 @@ public class TenantEntitlementConsumer {
     var applicationId = event.getApplicationId();
     if (shouldEnableTenant(event.getType())) {
       tenantService.enableTenant(tenantName, applicationId);
+      applicationManagerService.getModuleBootstrap(applicationId)
+        .onSuccess(bootstrap -> egressRoutingLookup.onApplicationBootstrap(applicationId,
+          bootstrap.getRequiredModules()))
+        .onFailure(err -> log.warn("Failed to load bootstrap for application {}: {}", applicationId, err.getMessage()));
       return;
     }
 
     tenantService.disableTenant(tenantName, applicationId);
+    if (tenantService.getAllApplicationIds().stream().noneMatch(applicationId::equals)) {
+      egressRoutingLookup.onApplicationRevoked(applicationId);
+    }
   }
 
   private boolean shouldEnableTenant(Type type) {
--- src/test/java/org/folio/sidecar/it/kafka/TenantEntitlementConsumerIT.java
+++ src/test/java/org/folio/sidecar/it/kafka/TenantEntitlementConsumerIT.java
@@ -1,11 +1,16 @@
 package org.folio.sidecar.it.kafka;
 
+import static io.vertx.core.Future.succeededFuture;
 import static org.awaitility.Durations.FIVE_SECONDS;
 import static org.awaitility.Durations.ONE_HUNDRED_MILLISECONDS;
 import static org.folio.sidecar.support.TestConstants.APPLICATION_ID;
+import static org.folio.sidecar.support.TestConstants.MODULE_BOOTSTRAP;
 import static org.folio.sidecar.support.TestConstants.MODULE_ID;
 import static org.folio.sidecar.support.TestConstants.TENANT_NAME;
 import static org.folio.sidecar.support.TestConstants.TENANT_UUID;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -18,12 +23,16 @@ import io.smallrye.reactive.messaging.memory.InMemoryConnector;
 import io.smallrye.reactive.messaging.memory.InMemorySource;
 import jakarta.enterprise.inject.Any;
 import jakarta.inject.Inject;
+import java.util.Collections;
+import java.util.Set;
 import org.awaitility.Awaitility;
 import org.awaitility.core.ThrowingRunnable;
+import org.folio.sidecar.integration.am.ApplicationManagerService;
 import org.folio.sidecar.integration.kafka.TenantEntitlementConsumer;
 import org.folio.sidecar.integration.kafka.TenantEntitlementEvent;
 import org.folio.sidecar.integration.kafka.TenantEntitlementEvent.Type;
 import org.folio.sidecar.service.TenantService;
+import org.folio.sidecar.service.routing.lookup.EgressRoutingLookup;
 import org.folio.sidecar.support.extensions.EnableWireMock;
 import org.folio.sidecar.support.extensions.InMemoryMessagingExtension;
 import org.folio.sidecar.support.profile.InMemoryMessagingTestProfile;
@@ -42,6 +51,8 @@ class TenantEntitlementConsumerIT {
 
   @InjectSpy TenantEntitlementConsumer consumer;
   @InjectMock TenantService tenantService;
+  @InjectMock ApplicationManagerService applicationManagerService;
+  @InjectMock EgressRoutingLookup egressRoutingLookup;
 
   @Inject
   @Any
@@ -52,22 +63,42 @@ class TenantEntitlementConsumerIT {
   void consume_positive_entitleOrUpgradeEvent(Type type) {
     var event = TenantEntitlementEvent.of(MODULE_ID, TENANT_NAME, TENANT_UUID, type, APPLICATION_ID);
     when(tenantService.isAssignedModule(MODULE_ID)).thenReturn(true);
+    when(applicationManagerService.getModuleBootstrap(APPLICATION_ID)).thenReturn(succeededFuture(MODULE_BOOTSTRAP));
 
     sendEvent(event);
 
     awaitUntilAsserted(() -> verify(consumer).consume(event));
     verify(tenantService).enableTenant(TENANT_NAME, APPLICATION_ID);
+    verify(applicationManagerService).getModuleBootstrap(APPLICATION_ID);
+    verify(egressRoutingLookup).onApplicationBootstrap(eq(APPLICATION_ID), anyList());
   }
 
   @Test
-  void consume_positive_revokeEvent() {
+  void consume_positive_revokeEvent_lastTenant_revokesEgressCache() {
     var event = TenantEntitlementEvent.of(MODULE_ID, TENANT_NAME, TENANT_UUID, Type.REVOKE, APPLICATION_ID);
     when(tenantService.isAssignedModule(MODULE_ID)).thenReturn(true);
+    // After disableTenant, no other tenant uses this applicationId
+    when(tenantService.getAllApplicationIds()).thenReturn(Collections.emptySet());
 
     sendEvent(event);
 
     awaitUntilAsserted(() -> verify(consumer).consume(event));
     verify(tenantService).disableTenant(TENANT_NAME, APPLICATION_ID);
+    verify(egressRoutingLookup).onApplicationRevoked(APPLICATION_ID);
+  }
+
+  @Test
+  void consume_positive_revokeEvent_otherTenantPresent_keepsEgressCache() {
+    var event = TenantEntitlementEvent.of(MODULE_ID, TENANT_NAME, TENANT_UUID, Type.REVOKE, APPLICATION_ID);
+    when(tenantService.isAssignedModule(MODULE_ID)).thenReturn(true);
+    // Another tenant still uses this applicationId
+    when(tenantService.getAllApplicationIds()).thenReturn(Set.of(APPLICATION_ID));
+
+    sendEvent(event);
+
+    awaitUntilAsserted(() -> verify(consumer).consume(event));
+    verify(tenantService).disableTenant(TENANT_NAME, APPLICATION_ID);
+    verify(egressRoutingLookup, never()).onApplicationRevoked(APPLICATION_ID);
   }
 
   private void sendEvent(TenantEntitlementEvent event) {
