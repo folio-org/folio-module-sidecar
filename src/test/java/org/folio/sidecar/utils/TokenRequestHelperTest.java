1d32d2007556b59bec155c800823e3712a7df075
pkjacob <121239864+pkjacob@users.noreply.github.com>
2026-06-18 10:56:24 -0400

    MODSIDECAR-182 - Migrate UMA Permission Checks to Response Mode Decision (#366)

--- src/test/java/org/folio/sidecar/utils/TokenRequestHelperTest.java
+++ src/test/java/org/folio/sidecar/utils/TokenRequestHelperTest.java
@@ -5,9 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.folio.sidecar.integration.cred.model.UserCredentials;
 import org.folio.sidecar.support.TestConstants;
-import org.junit.jupiter.api.Assertions;
+import org.folio.support.types.UnitTest;
 import org.junit.jupiter.api.Test;
 
+@UnitTest
 class TokenRequestHelperTest {
 
   private static final UserCredentials TEST_USER = UserCredentials.of("test", "testpwd");
@@ -17,9 +18,9 @@ class TokenRequestHelperTest {
     var actual = TokenRequestHelper.preparePasswordRequestBody(TestConstants.LOGIN_CLIENT_CREDENTIALS, TEST_USER);
 
     assertThat(actual).hasSize(5);
-    Assertions.assertEquals(TokenRequestHelper.PASSWORD_GRANT_TYPE, actual.get("grant_type"));
-    Assertions.assertEquals(TestConstants.LOGIN_CLIENT_CREDENTIALS.getClientId(), actual.get("client_id"));
-    Assertions.assertEquals(TestConstants.LOGIN_CLIENT_CREDENTIALS.getClientSecret(), actual.get("client_secret"));
+    assertEquals(TokenRequestHelper.PASSWORD_GRANT_TYPE, actual.get("grant_type"));
+    assertEquals(TestConstants.LOGIN_CLIENT_CREDENTIALS.getClientId(), actual.get("client_id"));
+    assertEquals(TestConstants.LOGIN_CLIENT_CREDENTIALS.getClientSecret(), actual.get("client_secret"));
     assertEquals("test", actual.get("username"));
     assertEquals("testpwd", actual.get("password"));
   }
@@ -29,20 +30,23 @@ class TokenRequestHelperTest {
     var actual = TokenRequestHelper.prepareClientRequestBody(TestConstants.LOGIN_CLIENT_CREDENTIALS);
 
     assertThat(actual).hasSize(3);
-    Assertions.assertEquals(TokenRequestHelper.CLIENT_CREDENTIALS_GRANT_TYPE, actual.get("grant_type"));
-    Assertions.assertEquals(TestConstants.LOGIN_CLIENT_CREDENTIALS.getClientId(), actual.get("client_id"));
-    Assertions.assertEquals(TestConstants.LOGIN_CLIENT_CREDENTIALS.getClientSecret(), actual.get("client_secret"));
+    assertEquals(TokenRequestHelper.CLIENT_CREDENTIALS_GRANT_TYPE, actual.get("grant_type"));
+    assertEquals(TestConstants.LOGIN_CLIENT_CREDENTIALS.getClientId(), actual.get("client_id"));
+    assertEquals(TestConstants.LOGIN_CLIENT_CREDENTIALS.getClientSecret(), actual.get("client_secret"));
   }
 
   @Test
-  void prepareRptRequestBody_positive() {
-    var actual =
-      TokenRequestHelper.prepareRptRequestBody(TestConstants.LOGIN_CLIENT_CREDENTIALS.getClientId(), "/foo/bar#GET");
+  void prepareUmaDecisionRequestBody_positive() {
+    var actual = TokenRequestHelper.prepareUmaDecisionRequestBody(
+      TestConstants.LOGIN_CLIENT_CREDENTIALS.getClientId(),
+      "/foo/bar#GET"
+    );
 
-    assertThat(actual).hasSize(3);
-    Assertions.assertEquals(TokenRequestHelper.RPT_GRANT_TYPE, actual.get("grant_type"));
+    assertThat(actual).hasSize(4);
+    assertEquals(TokenRequestHelper.RPT_GRANT_TYPE, actual.get("grant_type"));
     assertEquals("/foo/bar#GET", actual.get("permission"));
-    Assertions.assertEquals(TestConstants.LOGIN_CLIENT_CREDENTIALS.getClientId(), actual.get("audience"));
+    assertEquals(TestConstants.LOGIN_CLIENT_CREDENTIALS.getClientId(), actual.get("audience"));
+    assertEquals(TokenRequestHelper.DECISION_RESPONSE_MODE, actual.get("response_mode"));
   }
 
   @Test
@@ -51,8 +55,8 @@ class TokenRequestHelperTest {
 
     assertThat(actual).hasSize(4);
     assertEquals("refresh_token", actual.get("grant_type"));
-    Assertions.assertEquals(TestConstants.LOGIN_CLIENT_CREDENTIALS.getClientId(), actual.get("client_id"));
-    Assertions.assertEquals(TestConstants.LOGIN_CLIENT_CREDENTIALS.getClientSecret(), actual.get("client_secret"));
+    assertEquals(TestConstants.LOGIN_CLIENT_CREDENTIALS.getClientId(), actual.get("client_id"));
+    assertEquals(TestConstants.LOGIN_CLIENT_CREDENTIALS.getClientSecret(), actual.get("client_secret"));
     assertEquals("refreshtoken", actual.get("refresh_token"));
   }
 }
