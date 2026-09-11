c2918baa7562d8e4bc2187e77334e857645ecb52
Yauhen Vavilkin <yauhen_vavilkin@epam.com>
2026-09-11 11:34:10 +0200

    MODSIDECAR-211: add missing classes and test resources

---
+++ src/main/java/org/folio/sidecar/integration/users/configuration/property/ModUsersKeycloakCacheProperties.java
@@ -0,0 +1,20 @@
+package org.folio.sidecar.integration.users.configuration.property;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ApplicationScoped
+public class ModUsersKeycloakCacheProperties {
+
+  @ConfigProperty(name = "mod-users-keycloak.cache-expiration-seconds") int cacheExpirationSeconds;
+  @ConfigProperty(name = "mod-users-keycloak.cache-initial-capacity") int cacheInitialCapacity;
+  @ConfigProperty(name = "mod-users-keycloak.cache-max-capacity") int cacheMaxCapacity;
+}
